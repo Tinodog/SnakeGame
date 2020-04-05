de36e7bb1f682a26406f7fdcfbefcd6dcc96aa40
Fabian Cooper <32171118+Tinodog@users.noreply.github.com>
2020-04-05 15:49:26 -0400

    Add files via upload

---
+++ GameScene.swift
@@ -0,0 +1,22 @@
+//
+//  GameScene.swift
+//  SnakeGame
+//
+//  Created by Fabian Cooper on 4/5/20.
+//  Copyright © 2020 Fabian Cooper. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+
+        }
+
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
